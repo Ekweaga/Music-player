65b534a3cee0e9463da4aebffb815eb9e440510c
Ekweaga <hikay133@gmail.com>
2022-08-30 20:28:03 +0100

    displaying app depending on window.InnerWith

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 
 import './App.css';
 import Login from './components/Login';
-import {useEffect} from'react'
+import {useEffect,useState} from'react'
 import {useStateProvider} from './utilis/StateProvider'
 import {reducercases} from './utilis/constants'
 import Spotify from './components/Spotify';
 
 function App() {
   const [{token}, dispatch] = useStateProvider();
+  const [desktopview, setdesktop] = useState(false)
+
+  const checkScreen= ()=>{
+    const windowWith= window.innerWidth
+    if(windowWith <= 750){
+      setdesktop(true)
+     
+        return;
+
+    }
+    else{
+      setdesktop(false)
+    }
+  }
 
   useEffect(()=>{
     const hash = window.location.hash
-   
     if(hash){
       const token = hash.substring(1).split("&")[0].split("=")[1]
       
@@ -19,11 +32,22 @@ function App() {
 
     }
   },[token,dispatch])
+
+  useEffect(()=>{
+      checkScreen();
+      window.addEventListener("resize",checkScreen)
+  },[])
   return (
     <div className="App">
+
     {
-      token ? <Spotify/> : <Login/>
+      desktopview ? (<div style={{color:'white',height:'100vh',display:'flex', flexDirection:'column', alignItems:'center',justifyContent:'center'}}><p style={{color:'white'}}>You cannot access on mobile device</p><p>Switch to a desktop or larger screen</p></div>) : (
+        
+          token ? <Spotify/> : <Login/>
+        
+      )
     }
+  
     </div>
   );
 }
